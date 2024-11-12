a15c0521169aedace7aa6da287ffd2ac71e850d9
theCathub <leticiabmariotti@gmail.com>
2024-11-12 11:05:42 -0300

    Update

--- Atividade_API/atvAPI.js
+++ Atividade_API/atvAPI.js
@@ -3,3 +3,6 @@ let localidade = document.getElementById("idCidade");
 let bairro = document.getElementById("idBairro");
 let logradouro = document.getElementById("idRua");
 
+const link = "https://viacep.com.br/ws/" + estado + "/" + localidade + "/" + bairro + "/" + logradouro + "/json"
+let lista = link
+
