8282e6645bc87f514b899221ed2ded52582e2b98
theCathub <leticiabmariotti@gmail.com>
2024-11-12 10:56:24 -0300

    Update

---
+++ Atividade_API/atvAPI.js
@@ -0,0 +1,5 @@
+let estado = document.getElementById("idEstado");
+let localidade = document.getElementById("idCidade");
+let bairro = document.getElementById("idBairro");
+let logradouro = document.getElementById("idRua");
+
